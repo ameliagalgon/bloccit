647e36bc8f5e3e219106974b3591f48f9910a9cb
Amelia Galgon <agalgon@gmail.com>
2017-10-08 17:00:07 -0400

    Assignment 18 Intro to CRUD: complete. use update on @posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
       @posts = Post.all
+
+      #the posts should be updated by the controller
+      for i in 0..@posts.length
+          if i % 5 == 0
+              @posts[i].update(title: "SPAM")
+          end
+      end
   end
 
   def show

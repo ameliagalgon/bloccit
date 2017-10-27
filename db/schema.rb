30328e3a8f5b52f0c771eb5e66aa0d168847e64a
Amelia Galgon <agalgon@gmail.com>
2017-10-27 18:53:43 -0400

    more changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026231211) do
+ActiveRecord::Schema.define(version: 20171027164833) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -57,9 +57,11 @@ ActiveRecord::Schema.define(version: 20171026231211) do
     t.string "title"
     t.text "body"
     t.integer "price"
+    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "topic_id"
+    t.index ["question_id"], name: "index_sponsored_posts_on_question_id"
     t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 

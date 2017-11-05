7e54102c5523bbe58188227eefa97638acccecbb
Amelia Galgon <agalgon@gmail.com>
2017-11-05 10:53:46 -0500

    Checkpoint 25 Authentication Signing In: complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
           end
      end
 
+     resources :sessions, only: [:new, :create, :destroy]
+
      get 'about' => 'welcome#about'
 
      root 'welcome#index'

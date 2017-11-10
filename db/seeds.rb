251dc4253eb6560eb2f1acca4fdc1853d4107971
Amelia Galgon <agalgon@gmail.com>
2017-11-09 23:07:11 -0500

    got to Testing Association

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,17 @@
 
 require 'random_data'
 
+# Create Users
+5.times do
+     User.create!(
+          name: RandomData.random_name,
+          email: RandomData.random_email,
+          password: RandomData.random_sentence
+     )
+end
+users = User.all
+
+# Create Topics
 15.times do
      Topic.create!(
           name: RandomData.random_sentence,
@@ -17,8 +28,9 @@ end
 topics = Topic.all
 
 50.times do
-
+     # Create Posts
     Post.create!(
+         user: users.sample,
          topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
@@ -58,7 +70,14 @@ question = Question.all
     )
 end
 
+user = User.first
+user.update_attributes!(
+     email: "agalgon@gmail.com",
+     password: "helloworld"
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"

9d92521f54707a66a079790f816003b0cb869c8a
Amelia Galgon <agalgon@gmail.com>
2017-11-29 20:46:26 -0500

    sendgrid still not working

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,15 +1,16 @@
 class FavoriteMailer < ApplicationMailer
   default from: "agalgon@gmail.com"
+  default cc: "caila.blanton@bloc.io"
 
   def new_comment(user, post, comment)
-    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    headers["Message-ID"] = "<comments/#{comment.id}@nameless-island-88424>"
+    headers["In-Reply-To"] = "<post/#{post.id}@nameless-island-88424>"
+    headers["References"] = "<post/#{post.id}@nameless-island-88424>"
 
     @user = user
     @post = post
     @comment = comment
 
-    mail(to: user.email, subject: "New comment on #{post.title}")
+    mail(to: "ameliagalgon@gmail.com", subject: "New comment on #{post.title}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 end

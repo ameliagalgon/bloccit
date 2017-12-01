f083bfd68db6ceaee049d72a4b0e3e18ace5c349
Amelia Galgon <agalgon@gmail.com>
2017-11-30 22:31:10 -0500

    first attempt at assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
     has_many :favorites, dependent: :destroy
 
     after_create :create_vote
+    after_create :create_favorite
 
     default_scope { order('rank DESC') }
 
@@ -39,5 +40,10 @@ class Post < ApplicationRecord
 
     def create_vote
       user.votes.create(value: 1, post: self)
+    end
+
+    def create_favorite
+      user.favorites.create(post: self)
+      FavoriteMailer.new_post(user, self).deliver_now
     end
 end

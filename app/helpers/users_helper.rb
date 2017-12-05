f364668b4612e1537917c0462f7dd8837787c129
Amelia Galgon <agalgon@gmail.com>
2017-12-05 12:52:18 -0500

    Assignment 32 Public Profiles: complete.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def has_posts?
+    return @user.posts.size > 0
+  end
+
+  def has_comments?
+    return @user.comments.size > 0
+  end
+
+  def has_favorites?
+    return @user.favorites.size > 0
+  end
+
 end

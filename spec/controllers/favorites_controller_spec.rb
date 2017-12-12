0f38b36815e93e1c37af15015c86b524eff918ce
Amelia Galgon <agalgon@gmail.com>
2017-12-12 17:48:06 -0500

    fixed favorite controller specs

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'rails_helper'
 include SessionsHelper
 
@@ -5,6 +6,7 @@ RSpec.describe FavoritesController, type: :controller do
   let(:my_user) { create(:user) }
   let(:my_topic) { create(:topic) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:another_user) { create(:user) }
 
   context 'guest user' do
     describe 'POST create' do
@@ -25,7 +27,7 @@ RSpec.describe FavoritesController, type: :controller do
 
   context 'signed in user' do
     before do
-      create_session(my_user)
+      create_session(another_user)
     end
 
     describe 'POST create' do
@@ -34,25 +36,27 @@ RSpec.describe FavoritesController, type: :controller do
         expect(response).to redirect_to([my_topic, my_post])
       end
 
-      it 'create a favorite for the current user and specified post' do
-        expect(my_user.favorites.find_by_post_id(my_post.id)).to be_nil
+      it 'creates a favorite for the current user and specified post' do
+        #binding.pry
+
+        expect(another_user.favorites.find_by(post_id: my_post.id)).to be_nil
         post :create, params: {post_id: my_post.id}
-        expect(my_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
+        expect(another_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
       end
     end
 
     describe 'DELETE destroy' do
       it 'redirects to the posts show view' do
-        favorite = my_user.favorites.where(post:my_post).create
+        favorite = another_user.favorites.where(post:my_post).create
         delete :destroy, params: { post_id: my_post.id, id: favorite.id }
         expect(response).to redirect_to([my_topic, my_post])
       end
 
       it 'destroys the favorite for the current user and post' do
-        favorite = my_user.favorites.where(post: my_post).create
-        expect(my_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
+        favorite = another_user.favorites.where(post: my_post).create
+        expect(another_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
         delete :destroy, params: {post_id: my_post.id, id: favorite.id}
-        expect(my_user.favorites.find_by_post_id(my_post.id)).to be_nil
+        expect(another_user.favorites.find_by_post_id(my_post.id)).to be_nil
       end
     end
   end

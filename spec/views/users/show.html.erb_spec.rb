405118eef2dc3b22d1f6ce3a2c5beb7aec4e6c68
Amelia Galgon <agalgon@gmail.com>
2017-12-12 20:01:09 -0500

    trying to test that partial for favorite posts are rendering

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -6,13 +6,19 @@ RSpec.describe "users/show.html.erb", type: :view do
   let(:another_user) { create(:user) }
   let(:post) { create(:post, user_id: another_user.id) }
 
+=begin
   it "displays user favorites" do
-    assign(:user, [
-      stub_template(:id => user.id),
-    ])
-    Favorite.create(post_id: post.id, user_id: user.id)
-
-    expect(view).to render_template(:partial => "_post", :locals => { :post => post } )
+    Favorite.create!(post_id: post.id, user_id: user.id)
+    render :partial => "posts/post.html.erb", :locals => { :post => post }
+    expect(rendered).to match /HTML/
   end
+=end
 
+  it "displays user favorites" do
+    Favorite.create!(post_id: post.id, user_id: user.id)
+    assigns(:user, user)
+    render
+    expect(view).to render_template(:partial => 'posts/_post', :locals => { :post => post })
+  end
+  
 end

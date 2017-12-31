ed7f71319a44f797af085db42dbcd3a0891c3902
Amelia Galgon <agalgon@gmail.com>
2017-12-31 11:48:06 -0500

    change action mailer host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'nameless-island-88424.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'vast-thicket-98832.herokuapp.com' }
 end
